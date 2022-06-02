851f4ed20f7045c33c79ea5a5c807e55a583d298
DeoclecioFilho <deoclecio.filho@estudante.ufjf.br>
2022-06-01 22:08:28 -0300

    Adiciona passagem de dados entre activity

--- app/src/main/java/br/ufjf/dcc196/deocleciofilho/calculajuros/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/deocleciofilho/calculajuros/MainActivity.java
@@ -5,17 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editTextValorPrsente;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextValorPrsente = findViewById(R.id.editTextValorPrensente);
     }
 
-    public void abreJurosSimples(View view){
+    public void JurosSimplesClick(View view){
+        Double valorPresente = Double.parseDouble(editTextValorPrsente.getText().toString());
+
         Intent intent = new Intent(MainActivity.this,JurosSimplesActivity.class);
+        intent.putExtra("valorPresente",valorPresente);
         startActivity(intent);
     }
 }
